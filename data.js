a25de02ada022df2e0e1aa31c47e4e1807415071
Archit Yadav <111058823+archit7820@users.noreply.github.com>
2023-10-13 04:33:38 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,172 @@
+export const cards = [
+  {
+    id: 1,
+    title: "Ayurveda",
+    desc: "Harmony Through Natural Healing",
+    img: "https://www.flickr.com/photos/staengl/4106445329/ ",
+  },
+  {
+    id: 2,
+    title: "Yoga",
+    desc: "Balance and Wellness",
+    img: "https://drive.google.com/file/d/1Gb2SyBiK9DwG7qrHb8flmxByIPm8OBBc/view?usp=sharing",
+  },
+  {
+    id: 3,
+    title: "Unani",
+    desc: " Nature's Healing Wisdom",
+    img: "http://admin.risingkashmir.com/Source/News/userfiles/image/s_637622905163264282_Image_View_1_(21).png ",
+  },
+  {
+    id: 4,
+    title: "Siddha",
+    desc: "Ancient Healing Art",
+    img: "https://st1.thehealthsite.com/wp-content/uploads/2022/05/siddha.jpg?impolicy=Medium_Widthonly&w=710",
+  },
+  {
+    id: 5,
+    title: "Sowa Rigpa",
+    desc: "the Tibetan treasure",
+    img: "https://ayushnext.ayush.gov.in/thumb/thumb_719X395_Sowa-Rigpa.jpg",
+  },
+  {
+    id: 6,
+    title: "Homeopathy",
+    desc: "Nature's remedy, healing clutch",
+    img: "https://drive.google.com/file/d/1pEWT0TkNoH9rdi5DEhaPVOVfOB-JMzav/view?usp=sharing",
+  },
+ 
+];
+
+export const projects = [
+  {
+    id: 1,
+    img:"https://i.pinimg.com/originals/1a/d3/bd/1ad3bdfee61126942ded57299eee4b49.jpg ",
+ 
+    cat: "AyurGyan",
+    username: "Apply Now ",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/originals/f8/f3/d8/f8f3d8603b07ee8088d4491c7bd13889.jpg",
+    
+    cat: "Ayurswasthya",
+    username: "Apply Now",
+  },
+  {
+    id: 3,
+    img: "https://www.medgadget.com/wp-content/uploads/2019/09/Medical-Tourism.jpg" ,
+   
+    cat: "Champion Services sector scheme",
+    username: "Apply Now ",
+  },
+  {
+    id: 4,
+    img: "https://static.scientificamerican.com/blogs/cache/file/058C142D-9D32-4AD5-8F6A681135356C45_source.jpg?w=590&h=800&EAB82513-C041-42EB-ABB2EB88039BBD92",
+   
+    cat: "Promotion of International Co-operation",
+    username: "Apply Now ",
+  },
+  {
+    id: 5,
+    img: "https://arielcie.files.wordpress.com/2014/02/communicate2.png",
+   
+    cat: "Information, Eduction & Communication",
+    username: "Apply Now ",
+  },
+  {
+    id: 6,
+    img: "https://dindayalaushadhi.com/admin/blog_image/4qti6li1v9q8040.png",
+    cat: "IAyush Oushadhi gunvatha evum authpadan",
+    username: "Apply Now ",
+  },
+  {
+    id: 7,
+    img: "https://www.conserve-energy-future.com/wp-content/uploads/2014/07/sustainable-development-eco-friendly-globe.jpg",
+    
+    cat: "Conservation development and Sustainable management of medicinal plants",
+    username: "Apply Now ",
+  },
+  {
+    id: 8,
+    img: "https://pscprep.com/wp-content/uploads/2020/11/National-Ayush-Mission-NAM-1024x576.jpg",
+  
+    cat: "National Ayush Mission ",
+   username: "Apply Now ",
+  },
+];
+
+export const gigs = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/580151/pexels-photo-580151.jpeg?auto=compress&cs=tinysrgb&w=1600",
+   
+    desc: "I will create ai art character from your images and prompts",
+    price: 59,
+    star: 5,
+    username: "Anna Bell",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/4145190/pexels-photo-4145190.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    
+    desc: "I will create ultra high quality character art with ai",
+    price: 79,
+    star: 5,
+    username: "Lannie Coleman",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/8797307/pexels-photo-8797307.jpeg?auto=compress&cs=tinysrgb&w=1600",
+   
+    desc: "I will creating unique ai generated artworks mid journey ai artist",
+    price: 112,
+    star: 5,
+    username: "Carol Steve",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/5708069/pexels-photo-5708069.jpeg?auto=compress&cs=tinysrgb&w=1600",
+   
+    desc: "I will create custom ai generated artwork using your photos",
+    price: 99,
+    star: 4,
+    username: "Don Weber",
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/5699456/pexels-photo-5699456.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    
+    desc: "I will recreate your dreams in high quality pictures",
+    price: 59,
+    star: 5,
+    username: "Audrey Richards",
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/8100784/pexels-photo-8100784.jpeg?auto=compress&cs=tinysrgb&w=1600",
+   
+    desc: "I will create ai digital art illustration hyper realistic painting",
+    price: 79,
+    star: 4,
+    username: "Walton Shepard ",
+  },
+  {
+    id: 7,
+    img: "https://images.pexels.com/photos/6039245/pexels-photo-6039245.jpeg?auto=compress&cs=tinysrgb&w=1600",
+   
+    desc: "I will generate images with your prompts using ai dalle",
+    price: 89,
+    star: 5,
+    username: "Waverly Schaefer",
+  },
+  {
+    id: 8,
+    img: "https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&w=1600",
+   
+    desc: "I will create custom art using midjourney generator",
+    price: 110,
+    star: 4,
+    username: "Wilton Hunt",
+  },
+];
